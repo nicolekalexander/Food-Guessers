fbde593693c2cae4100eeee6604ffad4d588ef37
Nicole Alexander <nalexander245@gmail.com>
2020-01-19 12:06:51 -0500

    rounded food values and other minor edits

--- app.js
+++ app.js
@@ -14,13 +14,24 @@ const makeQuestion = () => {
     const foodAValue = randomFoodA[randomNutrient]
     const foodBValue = randomFoodB[randomNutrient]
 
-//jQuery selectors selecting buttons and writing the display names of the random foods
+    //if statement that calls make question if foodA or foodB value is 0
+    if (foodAValue == 0 || foodBValue == 0){
+        //make new question
+        makeQuestion()
+        //exit this function call
+        return false
+    }
+
+    //jQuery selectors selecting buttons and writing the display names of the random foods
     $("#foodA").text(randomFoodA.Display_Name)
     $("#foodB").text(randomFoodB.Display_Name)
     $("#nutrient").text(randomNutrient.replace("_", " "))
 
-//jQuery selector selecting buttons and assigning onclick event listener function 
-//when you click the selected buttons, the getNewQuestion function happens
+    //removes all event handlers in order to prevent over handling of events
+    $("#foodA, #foodB, #food0").off()
+
+    //jQuery selector selecting buttons and assigning onclick event listener function 
+    //when you click the selected buttons, the getNewQuestion function happens
     $("#foodA, #foodB, #food0").on("click" , () => {
         getNewQuestion({
             "nutrient": randomNutrient,
@@ -49,11 +60,8 @@ const getNewQuestion = (lastQuestionInfo) => {
     makeQuestion()
 }
 
-
-
-
+////////////////////////////////////////////////////////////////////////////////////////////
 //any code written after this runs once when user arrives, because it's in the global scope 
-
 
 //initially hides "last answer" row
 $("#lastAnswerRow").hide()
